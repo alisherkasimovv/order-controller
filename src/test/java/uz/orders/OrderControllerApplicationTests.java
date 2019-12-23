911f45000a2d1c409f8117c90e429ed8b821627c
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-12-23 12:33:41 +0500

    Initial commit

---
+++ src/test/java/uz/orders/OrderControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
