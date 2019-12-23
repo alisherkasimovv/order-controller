f5cbadcd0bcfde99e65433ad179e30d0e81c170d
Alisher Kasimov <alisherkasimov1994@gmail.com>
2019-12-23 12:45:51 +0500

    Created BaseEntity abstract class; Created UpdateBaseEntity abstarct class;

---
+++ src/main/java/uz/orders/db/entities/User.java
@@ -0,0 +1,4 @@
+package uz.orders.db.entities;
+
+public class User {
+}
---
+++ src/main/java/uz/orders/db/entities/base/BaseEntity.java
@@ -0,0 +1,30 @@
+package uz.orders.db.entities.base;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+//    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "[ id=" + getId() + "]";
+    }
+}
